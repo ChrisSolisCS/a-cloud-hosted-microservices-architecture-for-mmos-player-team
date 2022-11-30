7e8a017af133bf601901831aa051c71cfd48903d
Issac Lopez <lopezissaca@gmail.com>
2022-11-29 19:14:48 -0700

    fixed active and isActive error

--- src/main/java/edu/msudenver/profile/Profile.java
+++ src/main/java/edu/msudenver/profile/Profile.java
@@ -9,21 +9,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.NoSuchElementException;
-
-@Getter
-@Setter
+@Getter // Lombok annotation to generate getters
+@Setter // Lombok annotation to generate setters
 @RequiredArgsConstructor // EMPTY CONSTRUCTOR
-@Entity
+@Entity // JPA annotation to make this class an entity
 @Table(name = "profile")
 public class Profile {
-
+    // JPA annotation to make this field the primary key
     @Id
     @Column(name = "profile_id", columnDefinition = "SERIAL")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long profileId;
-
     @JsonIgnore
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "account_id", referencedColumnName = "account_id")
@@ -45,79 +42,13 @@ public class Profile {
     @Column(name = "is_active")
     private Boolean isActive;
 
-
-    public Profile(Long profileId, String profileName, Account account, String classType, String gender, String origins, Boolean is_active) {
+    public Profile(Long profileId, String profileName, Account account, String classType, String gender, String origins, Boolean isActive) {
         this.profileId = profileId;
         this.profileName = profileName;
         this.account = account;
         this.classType = classType;
         this.gender = gender;
         this.origins = origins;
-        this.isActive = is_active;
+        this.isActive = isActive;
     }
-
-//    public Profile() {
-//    }
-
-    public Long getProfileId() {
-        return profileId;
-    }
-
-    public void setProfileId(Long profileId) {
-        this.profileId = profileId;
-    }
-
-    public Account getAccount() {
-        return account;
-    }
-
-    public void assignAccount(Account account){
-        this.account = account;
-    }
-
-    public void setAccount(Account account) {
-        this.account = account;
-    }
-
-    public String getProfileName() {
-        return profileName;
-    }
-
-    public void setProfileName(String profileName) {
-        this.profileName = profileName;
-    }
-
-    public String getClassType() {
-        return classType;
-    }
-
-    public void setClassType(String classType) {
-        this.classType = classType;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public String getOrigins() {
-        return origins;
-    }
-
-    public void setOrigins(String origins) {
-        this.origins = origins;
-    }
-
-    public Boolean getActive() {
-        return isActive;
-    }
-
-    public void setActive(Boolean active) {
-        isActive = active;
-    }
-
-
 }
