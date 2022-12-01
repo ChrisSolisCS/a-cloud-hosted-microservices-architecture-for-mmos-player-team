ea24a33e69007ffda7cc5e72e27b7dc8c2392087
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2022-11-30 21:31:08 -0700

    Controller patch1
    Added Stats Controller v1

--- src/main/java/edu/msudenver/stats/StatsController.java
+++ src/main/java/edu/msudenver/stats/StatsController.java
@@ -1,4 +1,43 @@
 package edu.msudenver.stats;
 
+import edu.msudenver.characterSheet.CharacterSheet;
+import edu.msudenver.characterSheet.CharacterSheetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
 public class StatsController {
+    @Autowired
+    private StatsRepository StatsRepository;
+    //get all character stats
+    @GetMapping(produces = "application/json")
+    public ResponseEntity<List<Stats>> getAllStats() {
+        return ResponseEntity.ok(StatsRepository.findAll());
+    }
+
+    //get character stats by character name
+    @GetMapping(value = "/{characterName}", produces = "application/json")
+    public ResponseEntity<Stats> getStatsByCharacterName(@PathVariable String characterName) {
+        Stats characterStats = StatsRepository.getStatsByCharacterName(characterName); //this method will probably not work
+        return new ResponseEntity<>(characterStats, characterStats == null ? HttpStatus.NOT_FOUND : HttpStatus.OK);
+    }
+
+    //create a character stats
+    public Stats createStats(Stats characterStats) {
+        return StatsRepository.save(characterStats);
+    }
+
+    //delete a character stats
+    public void deleteStats(String characterName) {
+        StatsRepository.deleteById(characterName);
+    }
+    //patch a character stats
+    public Stats patchCharacterStats(Stats characterStats) {
+        return StatsRepository.save(characterStats);
+    }
+
 }
